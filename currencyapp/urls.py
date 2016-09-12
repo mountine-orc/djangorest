81e184d261bf8ddcbe6fc0de7c61484f29c93b8e
m.panko <m.panko@levi9.com>
2016-09-12 17:52:43 +0300

    Cleanup 3

--- currencyapp/urls.py
+++ currencyapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^importrates/$', views.importrates, name='importrates'),
-    url(r'^apicurency/$', views.RateList.as_view()),
-    url(r'^apicurencychanges/$', views.RateChanges.as_view()),
+    url(r'^api/curency/$', views.RateList.as_view()),
+    url(r'^api/curencychanges/$', views.RateChanges.as_view()),
 ]
