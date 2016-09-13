e833396402623ed8946c200323417a3e7424e1b5
m.panko <m.panko@levi9.com>
2016-09-13 17:49:53 +0300

    Add login page 2

--- currency35djproject/urls.py
+++ currency35djproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home/index.html'), name='home'),
+    url('^', include('django.contrib.auth.urls')),
     url(r'^currency/', include('currencyapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
