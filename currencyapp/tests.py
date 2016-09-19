e0997ad8c85033ae4f78a2c1db3c2faad1b584bf
m.panko <m.panko@levi9.com>
2016-09-19 15:08:07 +0300

    add pytest tests

--- currencyapp/tests.py
+++ currencyapp/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+@pytest.fixture()
+def user_setup(request):
+    return User.objects.create_user(username='temporary', email='jlennon@beatles.com', password='temporary')
+
+
+def test_not_logined(client):
+    response = client.get('/currency/')
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_logined(client, user_setup):
+    user = user_setup
+    client.force_login(user)
+    response = client.get('/currency/')
+    assert response.status_code == 200
+    assert b'Company' in response.content
